36bd9a69460d516a6c01bca47c60473386a473b1
Ian <>
2020-03-29 19:01:43 -0300

    Initial

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.json({ running: 'true' })
+})
+
+app.listen(port, () => {
+    console.log(`express is running on ${port}`)
+})
+
+console.log('oi')
